9adfd8db7f8479a02eddf3a46879cf3c82ac7d09
nomoreFt <hyunwoo.kim1108@gmail.com>
2022-03-27 19:52:03 +0900

    commit IOC DI 실습

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -5,10 +5,18 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 
 import hello.core.member.Member;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+/*
+Spring 이전 방법
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+*/
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
 
         Member memberA = new Member(1L, "memberA", Grade.VIP);
         memberService.join(memberA);//회원가입
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,7 +4,11 @@ public class MemberServiceImpl implements MemberService{
 
     //추상화에도 의존, 구체화에도 의존 (DIP 위반)
     //구현체가 변경될 때 마다 Client 코드를 변경해야하기 때문에 OCP도 위반
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
